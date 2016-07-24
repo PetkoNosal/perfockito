7eafef9607db892e08298c9c1619a04fafba6797
PetkoNosal <petonosal@gmail.com>
2016-07-24 21:03:15 +0200

    updated test

--- src/test/java/org/perfockito/reference/ReferenceTest.java
+++ src/test/java/org/perfockito/reference/ReferenceTest.java
@@ -4,18 +4,23 @@ import org.junit.Test;
 import org.perfockito.api.Perfockito;
 import org.perfockito.engine.PerfockitoEngineFactory;
 
-import java.util.concurrent.TimeUnit;
-
 public class ReferenceTest {
 
     @Test
     public void instanceReferenceTest() throws Exception {
+        long time = System.nanoTime();
+        testMethod1();
+        System.out.println("Method1 time: " + ((double)(System.nanoTime() - time) / 1000000000.0));
+
+        time = System.nanoTime();
+        testMethod2();
+        System.out.println("Method2 time: " + ((double)(System.nanoTime() - time) / 1000000000.0));
+
         Perfockito perfockito = PerfockitoEngineFactory.create("test");
-        perfockito.init(5, 10);
+        perfockito.init(10, 5);
         OneSecondWaitReference ref = new OneSecondWaitReference();
         perfockito.addStaticMethodToTest("testMethod1", this.getClass());
         perfockito.addStaticMethodToTest("testMethod2", this.getClass());
-        perfockito.addStaticMethodToTest("testMethod3", this.getClass());
         perfockito.runTest();
         perfockito.getReport().print();
     }
@@ -23,26 +28,16 @@ public class ReferenceTest {
 
 
     private static void testMethod1() {
-        try {
-            Thread.sleep(TimeUnit.MILLISECONDS.toMillis(500));
-        }catch (Exception e){
-            System.err.println(e);
+        for (int i = 0; i < 100; i++) {
+            Object o = new Object().equals(new Object());
         }
     }
 
     private static void testMethod2() {
-        try {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(3));
-        }catch (Exception e){
-            System.err.println(e);
-        }
-    }
-
-    private static void testMethod3() {
-        try {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(5));
-        }catch (Exception e){
-            System.err.println(e);
+        Object o1= new Object();
+        Object o2 = new Object();
+        for (int i = 0; i < 100; i++) {
+            o1.equals(o2);
         }
     }
 }
